c5d6cc83bae238d38ab2f447cddb77befd9c51bd
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-11-08 17:40:29 +0700

    FCM single code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,14 +6,17 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
+  providers: [FCM],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private fcm: FCM, private platform: Platform) {
+    this.platform.ready().then(() => {
+      this.initFCM();
+    })
+    .catch(e => console.log('ErrorInitFCM', e));
+  }
+
+  initFCM() {
+    this.fcm.getToken().then(token => {
+      // backend.registerToken(token);
+      console.log('token:',token);
+    });
+
+    this.fcm.onNotification().subscribe(data => {
+      if(data.wasTapped){
+        console.log('Received in background');
+      } else {
+        console.log('Received in foreground');
+      };
+    });
+
+    // this.fcm.onTokenRefresh().subscribe(token => {
+    //   console.log('token:',token);
+    // });
+
+    this.fcm.hasPermission().then(hasPermission => {
+      if (hasPermission) {
+        console.log('Has permission!');
+      }
+    });
+  }
+
+  subscribeToTopic() {
+    this.fcm.subscribeToTopic('HoangDuc');
+  }
+
+  unSubscribeToTopic() {
+    this.fcm.unsubscribeFromTopic('HoangDuc');
+  }
+
 
 }
